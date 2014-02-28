7dae7149b00d9300b65611186aa07d60b1069732
Jesse Kipp <jesse.kipp@huffingtonpost.com>
2014-02-28 09:55:43 -0500

    Namespaces js inside LabsPhotoWall
    On document ready, calls LabsPhotoWall.initialize, which takes an
    object with keys $source, $template, and $section which are jQuery
    selectors for the JSON, Handlebars template, and container,
    respectivley. It then calls LabsPhotoWall.populateGrid.

--- grid.js
+++ grid.js
@@ -1,119 +1,123 @@
-var tileTemplate = Handlebars.compile($('#template-tile').html());
+(function(root) {
 
-var kittehs = $.parseJSON($('#pictures').text());
+  var PW = root.LabsPhotoWall = (root.LabsPhotoWall || {});
 
-var _arrays = _.partition(kittehs, function(kitteh) {
-  return kitteh.importance > 0;
-});
-
-var important = _arrays[0], filler = _arrays[1];
-important.sort(function(firstKitteh, secondKitteh) {
-  return firstKitteh.importance < secondKitteh.importance;
-});
-
-var fillerRatio = function() {
-  return  filler.length / important.length;
-}
-
-var rand = (function() {
-  // Random function remembers previous returned value
-  // so it won't return the same value twice.
-  var lastValue = {};
-  var memoRand = function(n) {
-    return Math.ceil(Math.random() * n);
+  var fillerRatio = function() {
+    return  PW.filler.length / PW.important.length;
   }
-  return function(n) {
-    if (n < 1) return undefined;
-    else if (n === 1) return 1;
 
-    var newValue = memoRand(n);
-    while (lastValue[n] == newValue) {
-      newValue = memoRand(n);
+  var rand = (function() {
+    // Random function remembers previous returned value
+    // so it won't return the same value twice.
+    var lastValue = {};
+    var memoRand = function(n) {
+      return Math.ceil(Math.random() * n);
     }
-    lastValue[n] = newValue;
-    return newValue;
-  }
-})();
+    return function(n) {
+      if (n < 1) return undefined;
+      else if (n === 1) return 1;
 
-var randomColor = function() {
-  switch (rand(5)) {
-  case 1:
-    return "c1";
-  case 2:
-    return "c2";
-  case 3:
-    return "c3";
-  case 4:
-    return "c4";
-  case 5:
-    return "c5";
-  }
-}
-
-var renderSingleCell = function(arr) {
-  var el = arr.splice(0,1);
-  var $cell = $('<div class="cell">');
-  if (el[0] !== undefined) {
-    var e = el[0];
-    e.color = randomColor();
-    $cell.append(tileTemplate(e));
-  }
-  return $cell;
-}
-
-var renderDoubleCell = function(arr) {
-  var els = arr.splice(0,2);
-  var $cell = $('<div class="cell">');
-  while (els.length > 0) {
-    var e = els.splice(0,1)[0];
-    e.color = randomColor();
-    $cell.append(tileTemplate(e));
-  }
-  return $cell;
-}
-
-var placeOnlyLargePhotos = function() {
-  var $ss = $('#grid-container');
-  $ss.append(renderSingleCell(important));
-  $ss.append(renderSingleCell(important));
-}
-
-var placeOnlySmallPhotos = function() {
-  var $ss = $('#grid-container');
-  if (filler.length >= 8) {
-    var i = 0;
-    for (i=0; i < 4; i++) {
-      $ss.append(renderDoubleCell(filler));
+      var newValue = memoRand(n);
+      while (lastValue[n] == newValue) {
+        newValue = memoRand(n);
+      }
+      lastValue[n] = newValue;
+      return newValue;
     }
-  } else {
-    while (filler.length > 0) {
-      $ss.append(renderSingleCell(filler));
-    }
-  }
-}
-var placeMixOfPhotos = function() {
-  var $ss = $('#grid-container');
-  switch (rand(3)) {
+  })();
+
+  var randomColor = function() {
+    switch (rand(5)) {
     case 1:
-      $ss.append(renderSingleCell(important));
-      $ss.append(renderDoubleCell(filler));
-      $ss.append(renderDoubleCell(filler));
-      break;
+      return "c1";
     case 2:
-      $ss.append(renderDoubleCell(filler));
-      $ss.append(renderSingleCell(important));
-      $ss.append(renderDoubleCell(filler));
-      break;
+      return "c2";
     case 3:
-      $ss.append(renderDoubleCell(filler));
-      $ss.append(renderDoubleCell(filler));
-      $ss.append(renderSingleCell(important));
-      break;
+      return "c3";
+    case 4:
+      return "c4";
+    case 5:
+      return "c5";
+    }
   }
-}
 
-var populateGrid = function() {
-  while (fillerRatio() > 4) placeOnlyLargePhotos();
-  while (important.length > 0) placeMixOfPhotos();
-  while (filler.length > 0) placeOnlySmallPhotos();
-}
+  PW.renderSingleCell = function(arr) {
+    var el = arr.splice(0,1);
+    var $cell = $('<div class="cell">');
+    if (el[0] !== undefined) {
+      var e = el[0];
+      e.color = randomColor();
+      $cell.append(PW.tileTemplate(e));
+    }
+    return $cell;
+  }
+
+  PW.renderDoubleCell = function(arr) {
+    var els = arr.splice(0,2);
+    var $cell = $('<div class="cell">');
+    while (els.length > 0) {
+      var e = els.splice(0,1)[0];
+      e.color = randomColor();
+      $cell.append(PW.tileTemplate(e));
+    }
+    return $cell;
+  }
+
+  var placeOnlyLargePhotos = function() {
+    PW.$ss.append(PW.renderSingleCell(PW.important));
+    PW.$ss.append(PW.renderSingleCell(PW.important));
+  }
+
+  var placeOnlySmallPhotos = function() {
+    if (PW.filler.length >= 8) {
+      var i = 0;
+      for (i=0; i < 4; i++) {
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+      }
+    } else {
+      while (PW.filler.length > 0) {
+        PW.$ss.append(PW.renderSingleCell(PW.filler));
+      }
+    }
+  }
+  var placeMixOfPhotos = function() {
+    switch (rand(3)) {
+      case 1:
+        PW.$ss.append(PW.renderSingleCell(PW.important));
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+        break;
+      case 2:
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+        PW.$ss.append(PW.renderSingleCell(PW.important));
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+        break;
+      case 3:
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+        PW.$ss.append(PW.renderDoubleCell(PW.filler));
+        PW.$ss.append(PW.renderSingleCell(PW.important));
+        break;
+    }
+  }
+
+  PW.populateGrid = function() {
+    while (fillerRatio() > 4) placeOnlyLargePhotos();
+    while (PW.important.length > 0) placeMixOfPhotos();
+    while (PW.filler.length > 0) placeOnlySmallPhotos();
+  }
+
+  PW.initialize = function(options) {
+    PW.$ss = options.$section;
+    PW.tileTemplate = Handlebars.compile(options.$template.html());
+    var kittehs = $.parseJSON(options.$source.text());
+    var _arrays = _.partition(kittehs, function(kitteh) {
+      return kitteh.importance > 0;
+    });
+
+    PW.important = _arrays[0].sort(function(firstKitteh, secondKitteh) {
+      return firstKitteh.importance < secondKitteh.importance;
+    });
+    PW.filler = _arrays[1];
+  }
+
+}(this));
