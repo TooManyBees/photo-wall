40bd5b9431cd67229db0bc342668bd27849aa9bd
Jesse Kipp <jesse.kipp@huffingtonpost.com>
2014-03-02 01:45:15 -0500

    Enables serializing inputs into json

--- public/build.js
+++ public/build.js
@@ -1,28 +1,77 @@
 $(document).ready(function() {
   var listTemplate = Handlebars.compile($('#list-template').text());
 
+  var $selector = $('#bucket-selector');
+  var $images = $('#image-list ul');
+  var $build = $('#builder');
+  var $button = $('#build-button');
+  var $output = $('#build-output iframe').contents().find('body');
+
   var getImagesFromBucket = function(bucket) {
+    var $imgLi = $('#image-list');
     $.ajax({
       url: '/api/bucket/'+bucket,
       dataType: 'json',
-      success: function(data, status) {
+      beforeSend: function() {
+        $images.empty();
+        $imgLi.addClass('working');
+        $selector.prop('disabled', true);
+      },
+      success: function(data) {
         populateImageList(data);
+        $build.addClass('show');
+      },
+      complete: function() {
+        $imgLi.removeClass('working');
+        $selector.prop('disabled', false);
       }
     });
   }
 
   var populateImageList = function(images) {
-    $ul = $('#image-list');
-    $ul.empty();
-    $ul.append('<p>Check photos as "Important" to make them appear large.</p>');
     _.each(images, function(image) {
-      $ul.append(listTemplate({"path": image}));
+      $images.append(listTemplate({"path": image}));
     });
   }
 
-  $('#bucket-selector').on('change', function() {
-    var bucket = $(this).val();
-    if (bucket !== "") getImagesFromBucket(bucket);
-    else $('#image-list').empty();
-  })
+  var serializeImages = function($ul) {
+    var serialized = [];
+    var $li = $ul.find('li');
+    $li.each(function() {
+      var $inputs = $(this).find('input');
+      var obj = {};
+      $inputs.each(function() {
+        if ($(this).val() !== "")
+        obj[$(this).prop('name')] = $(this).val();
+      });
+      serialized.push(obj);
+    });
+    return serialized;
+  }
+
+  $selector.on('change', function() {
+    var bucket = $selector.val();
+    if (bucket !== "") {
+      getImagesFromBucket(bucket);
+    } else {
+      $images.empty();
+      $build.removeClass('show');
+    }
+  }); // end $bucket-selector.on
+
+  $button.on("click", function() {
+    $(this).prop('disabled', true).text("Building...");
+    $
+    var $pre = $('<pre>').text(
+      JSON.stringify(
+        serializeImages($images),
+        null,
+        "  "
+      )
+    );
+    $output.empty();
+    $output.append($pre);
+    $(this).prop('disabled', false).text("Build JSON!");
+  }); // end $button.on
+
 });
