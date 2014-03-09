44c759fa53335c3c60fdec69d1659cb37c5b9575
Jesse Kipp <jesse.kipp@huffingtonpost.com>
2014-03-08 19:17:17 -0500

    Modularizes the builder js

--- photoadmin/public/bucket_build.js
+++ photoadmin/public/bucket_build.js
@@ -1,29 +1,61 @@
-var setupBuilder = function() {
-  var $images = $('#image-list ul');
-  var $form = $('#build-form');
-  var $output = $('#build-output iframe').contents().find('body');
-  var currentBucket = $('#bucket-name').text();
+(function(root, $) {
 
-  $images.find('img').each(function() {
-    var $img = $(this);
-    $img.on('load', function() {
-      var height = $img.height();
-      var width = $img.width();
-      if (height / width === 1) {
-        $img.addClass('tall');
-        $img.parent().find('.controls').hide();
-        $img.parent().find('input[name=dim]').val('tall');
-      } else if (height / width > 1) {
-        $img.addClass('tall');
-        $img.parent().find('.horizontal').hide();
-        $img.parent().find('input[name=dim]').val('tall');
-      } else {
-        $img.addClass('wide');
-        $img.parent().find('.vertical').hide();
-        $img.parent().find('input[name=dim]').val('wide');
-      }
+  var Builder = root.Builder = (root.Builder || {});
+
+  Builder.setup = function() {
+    var $images = $('#image-list ul');
+    var $form = $('#build-form');
+    var $output = $('#build-output iframe').contents().find('body');
+
+    $images.find('img').each(function() {
+      var $img = $(this);
+      $img.on('load', function() {
+        var height = $img.height();
+        var width = $img.width();
+        if (height / width === 1) {
+          $img.addClass('tall');
+          $img.parent().find('.controls').hide();
+          $img.parent().find('input[name=dim]').val('tall');
+        } else if (height / width > 1) {
+          $img.addClass('tall');
+          $img.parent().find('.horizontal').hide();
+          $img.parent().find('input[name=dim]').val('tall');
+        } else {
+          $img.addClass('wide');
+          $img.parent().find('.vertical').hide();
+          $img.parent().find('input[name=dim]').val('wide');
+        }
+      });
     });
-  });
+
+    $form.on("submit", function(event) {
+      event.preventDefault();
+      var serialized = serializeImages($images);
+      var generatedJSON = JSON.stringify(serialized, null, "  ");
+      var $pre = $('<pre>').text(generatedJSON);
+      $output.empty();
+      $output.append($pre);
+      $('#build-output').removeClass('off');
+
+      $.ajax({
+        url: $(this).attr('action'),
+        dataType: 'json',
+        type: 'POST',
+        data: {
+          json: generatedJSON
+        },
+        success: function(data) {
+          $('#saved-url').text(data.url).attr('href', "/test/?src="+data.url);
+        }
+      })
+    });
+
+    $images.on('click', '.controls', function() {
+      var $control = $(this);
+      var $border = $(this).parent();
+      applyOffset($(this).parents('li'), {x: $control.data('x'), y: $control.data('y')})
+    });
+  }
 
   var serializeImages = function($ul) {
     var serialized = [];
@@ -44,34 +76,6 @@ var setupBuilder = function() {
     return serialized;
   }
 
-  $form.on("submit", function(event) {
-    event.preventDefault();
-    var serialized = serializeImages($images);
-    var generatedJSON = JSON.stringify(serialized, null, "  ");
-    var $pre = $('<pre>').text(generatedJSON);
-    $output.empty();
-    $output.append($pre);
-    $('#build-output').removeClass('off');
-
-    $.ajax({
-      url: $(this).attr('action'),
-      dataType: 'json',
-      type: 'POST',
-      data: {
-        json: generatedJSON
-      },
-      success: function(data) {
-        $('#saved-url').text(data.url).attr('href', "/test/?src="+data.url);
-      }
-    })
-  }); // end $button.on
-
-  $images.on('click', '.controls', function() {
-    var $control = $(this);
-    var $border = $(this).parent();
-    applyOffset($(this).parents('li'), {x: $control.data('x'), y: $control.data('y')})
-  });
-
   var applyOffset = function(thisVal, offset) {
     var $border = thisVal.find('.border');
     var $inputX = thisVal.find('input[name=dimX]');
@@ -84,15 +88,16 @@ var setupBuilder = function() {
 
     shiftBorder($border, offsetX, offsetY);
   }
-}
 
-var shiftBorder = function($border, offsetX, offsetY) {
-  $border.css('left', parseInt($border.css('left')) - offsetX);
-  $border.css('top', parseInt($border.css('top')) - offsetY);
-}
+  var shiftBorder = function($border, offsetX, offsetY) {
+    $border.css('left', parseInt($border.css('left')) - offsetX);
+    $border.css('top', parseInt($border.css('top')) - offsetY);
+  }
 
-var matchBorderToInputValues = function($li) {
-  var offsetX = parseInt($li.find('input[name=dimX]').val());
-  var offsetY = parseInt($li.find('input[name=dimY]').val());
-  shiftBorder($li.find('.border'), offsetX, offsetY);
-}
+  Builder.matchBorderToInputValues = function($li) {
+    var offsetX = parseInt($li.find('input[name=dimX]').val());
+    var offsetY = parseInt($li.find('input[name=dimY]').val());
+    shiftBorder($li.find('.border'), offsetX, offsetY);
+  }
+
+}(this, jQuery));
