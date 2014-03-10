00cbd584bfcd030c2a04e7781c63f36eace57310
Jesse Kipp <jesse.kipp@huffingtonpost.com>
2014-03-10 14:06:33 -0400

    Adds drag-and-drop uploading of multiple images

--- photoadmin/public/ajaxupload.js
+++ photoadmin/public/ajaxupload.js
@@ -1,5 +1,6 @@
 // ajax uploads
 // https://github.com/redmovil-mobi/ajaxupload.js
+// mildly modified
 (function($){
 
   function supportAjaxUploadWithProgress() {
@@ -50,23 +51,25 @@
       return;
     }
 
+    settings.before(files);
+
     if(settings.single){
 
       var count    = files.length,
         uploaded = 0,
         complete = settings.complete;
 
-        settings.complete = function(response, xhr){
-          uploaded = uploaded+1;
-          complete(response, xhr);
-          if(uploaded<count){
-            upload([files[uploaded]], settings);
-          }else{
-            settings.allcomplete();
-          }
-        };
+      settings.complete = function(response, xhr){
+        uploaded = uploaded+1;
+        complete(response, xhr);
+        if(uploaded<count){
+          upload([files[uploaded]], settings);
+        }else{
+          settings.allcomplete();
+        }
+      };
 
-        upload([files[0]], settings);
+      upload([files[0]], settings);
 
     } else {
 
@@ -78,8 +81,6 @@
       // upload all at once
       var formData = new FormData(),
         xhr      = new XMLHttpRequest();
-
-      settings.before(settings);
 
       for (var i = 0, f; f = files[i]; i++) {
         formData.append("files[]", f);
@@ -156,83 +157,6 @@
       }).on("dragover", function(e){
         e.stopPropagation();
         e.preventDefault();
-      });
-    });
-  };
-
-  $.fn.ajaxform = function(options){
-
-    if(!$.support.ajaxupload){
-      return this;
-    }
-
-    return this.each(function(){
-
-      var form     = $(this),
-        settings = $.extend({
-          "action": form.attr("action"),
-          "method": form.attr("method"),
-
-          // events
-          "loadstart": function(){},
-          "load": function(){},
-          "loadend": function(){},
-          "progress": function(){},
-          "complete": function(){},
-          "readystatechange": function(){}
-        }, options);
-
-      form.on("submit", function(e){
-
-        e.preventDefault();
-
-        var formData = new FormData(this),
-          xhr      = new XMLHttpRequest();
-
-        formData.append("formdata", "1");
-
-        // Add any event handlers here...
-        xhr.upload.addEventListener("progress", function(e){
-          var percent = (e.loaded / e.total)*100;
-          settings.progress(percent, e);
-        }, false);
-        xhr.addEventListener("loadstart", function(e){
-          settings.loadstart(e);
-        }, false);
-        xhr.addEventListener("load", function(e){
-          settings.load(e);
-        }, false);
-        xhr.addEventListener("loadend", function(e){
-          settings.loadend(e);
-        }, false);
-        xhr.addEventListener("error", function(e){
-          settings.error(e);
-        }, false);
-        xhr.addEventListener("abort", function(e){
-          settings.abort(e);
-        }, false);
-
-        xhr.open(settings.method, settings.action, true);
-        xhr.onreadystatechange = function() {
-
-          settings.readystatechange(xhr);
-
-          if(xhr.readyState==4){
-
-            var response = xhr.responseText;
-
-            if(settings.type=="json") {
-              try{
-                response = $.parseJSON(response);
-              }catch(e){
-                response = false;
-              }
-            }
-
-            settings.complete(response, xhr);
-          }
-          };
-          xhr.send(formData);
       });
     });
   };
--- photoadmin/public/upload.js
+++ photoadmin/public/upload.js
@@ -1,20 +1,65 @@
 $(document).ready(function() {
   var $dropScreen = $('#drop-screen');
   var $dropTarget = $('#drop-target');
+  var bucket = $dropTarget.data('bucket');
 
   $('#click-to-upload').on('click', function() {
     $dropScreen.show();
   });
 
   $('#hide-upload').on('click', function() {
-    $dropScreen.hide();
+    closeUploader($dropScreen);
   });
   $dropScreen.on('click', function() {
     $dropScreen.hide();
   });
   $dropTarget.on('click', function(e) {
-    e.originalEvent.stopPropagation();
-  })
+    e.stopPropagation();
+  });
 
   var _fileTemplate = Handlebars.compile($('#file-template').text());
+
+  $dropTarget.uploadOnDrag({
+    action: '/api/buckets/'+bucket,
+    single: true,
+    method: 'POST',
+    params: {},
+
+    before: function(files) {
+      _.each(files, function(file) {
+        var $li = $('<li class="file-name">')
+          .text(file.name)
+          .attr('data-name', file.name)
+          .append('<span class="status">');
+        $dropTarget.find('.file-list').append($li);
+      });
+    },
+    progress: function(){},
+    complete: function(response, xhr) {
+      response = JSON.parse(response);
+
+      var statusCode = xhr.status;
+
+      $status = $dropTarget.find('li[data-name="'+response.filename+'"] .status');
+      if (statusCode === 200) {
+        $status.addClass('success').text('Uploaded successfully.');
+      } else {
+        $status.addClass('error').text(response.error);
+      }
+    },
+    error: function(e) {
+      console.log("ERROR!");
+      console.log(e);
+    },
+    allcomplete: function(){
+      // closeUploader($dropScreen);
+      // populate the image grid with newly uploaded pics
+    }
+  });
+
+  var closeUploader = function($uploader) {
+    $uploader.hide();
+    $uploader.find('.file-list').empty();
+  }
+
 });
